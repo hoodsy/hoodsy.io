bf966ee7a0d940b0f83fdb040452f284808d9ca6
Greg McKeever <gmckeever@gmail.com>
2014-08-07 19:01:39 +0000

    Update Gulpfile tasks and fix AutoReload Issue

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,19 +4,19 @@ var gulp = require('gulp'),
         minifycss = require('gulp-minify-css'),
         rename = require('gulp-rename');
 
-function startExpress() {
+gulp.task('express', function() {
   var express = require('express');
   var app = express();
   app.use(require('connect-livereload')({port: 4002}));
   app.use(express.static(__dirname));
   app.listen(4000);
-}
+});
 
 var tinylr;
-function startLiveReload() {
+gulp.task('livereload', function() {
   tinylr = require('tiny-lr')();
   tinylr.listen(4002);
-}
+});
 
 function notifyLiveReload(event) {
   var fileName = require('path').relative(__dirname, event.path);
@@ -38,9 +38,12 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', function () {
-  startExpress();
-  startLiveReload();
-  gulp.watch('*.html/*.css', notifyLiveReload);
+gulp.task('watch', function() {
   gulp.watch('sass/*.scss', ['styles']);
+  gulp.watch('*.html', notifyLiveReload);
+  gulp.watch('css/*.css', notifyLiveReload);
+});
+
+gulp.task('default', ['styles', 'express', 'livereload', 'watch'], function() {
+
 });
